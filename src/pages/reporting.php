417436aa92e12a096746705c44bc1c71123947c7
TigerOkais <maximilian.rubein@endobit.de>
2019-07-02 15:11:47 +0200

    added select for hard and software

--- src/pages/reporting.php
+++ src/pages/reporting.php
@@ -3,7 +3,27 @@
      * Anzeige der verschiedenen Komponenten, abhängig vom ausgewähltem Raum
      */
 
-     $rooms = mysqli_query($dbLink, "SELECT RoomId, RoomNo FROM Rooms");
+    function getComponentByTypeId($typeId) {
+        global $dbLink;
+
+        $sqlStatement = 
+        "SELECT 
+        C.ComponentId AS ComponentId,
+        C.ComponentName,
+        CT.ComponentTypeName AS Kategorie,
+        R.RoomNo AS Raum
+        FROM Components AS C
+        INNER JOIN ComponentTypes AS CT ON C.ComponentTypeId = CT.ComponentTypeId
+        INNER JOIN ComponentsInRoom AS CR ON C.ComponentId = CR.ComponentId
+        INNER JOIN Rooms AS R ON R.RoomId = CR.RoomId        
+        WHERE CT.ComponentTypeId =" .$typeId. ";";
+
+        $result = mysqli_query($dbLink, $sqlStatement);
+        return $result;
+    }
+
+
+   
 
      /**
       * Gibt das Ergebnis der Datenbankabfrage - abhaengig von der RaumId - zurueck
@@ -31,7 +51,38 @@
         $result = mysqli_query($dbLink, $sqlStatement);
         return $result;
      }
+    /**
+    * Gibt alle Arten von Softwares zurueck
+    */
+    function getSoftwareTypes() {
+        global $dbLink;
+        $sqlStatement =
+        "SELECT ComponentTypeID, ComponentTypeName
+        FROM ComponentTypes
+        WHERE IsSoftware = 1";
 
+        $result = mysqli_query($dbLink, $sqlStatement);
+        return $result;
+    }
+
+    /**
+     * Gibt alle Arten von Hardwares zurueck
+    */
+    function getHardwareTypes() {
+        global $dbLink;
+        $sqlStatement =
+        "SELECT ComponentTypeID, ComponentTypeName
+        FROM ComponentTypes
+        WHERE IsSoftware = 0";
+
+        $result = mysqli_query($dbLink, $sqlStatement);
+        return $result;
+    }
+
+
+    $rooms = mysqli_query($dbLink, "SELECT RoomId, RoomNo FROM Rooms");
+    $hardware = getHardwareTypes();
+    $software = getSoftwareTypes();
      
 ?>
 
@@ -41,11 +92,27 @@
 <div class="card">
     <div class="card-body">
         <form method="POST">
-        <select name="selection" class="custom-select custom-select-lg" style="width: 10%" >
+        <select name="room" placeholder="Raum" class="custom-select custom-select-lg" style="width: 10%" >
             <option selected disabled>------</option>
             <?php 
                 foreach($rooms as $option) {
                     echo "<option value='", $option["RoomId"],"' > ", $option["RoomNo"], " </option>";
+                }        
+            ?>
+        </select>
+        <select name="hardware" placeholer="Hardware" class="custom-select custom-select-lg" style="width: 10%" >
+            <option selected disabled>------</option>
+            <?php 
+                foreach($hardware as $option) {
+                    echo "<option value='", $option["ComponentTypeID"],"' > ", $option["ComponentTypeName"], " </option>";
+                }        
+            ?>
+        </select>
+        <select name="software" placeholder="Software" class="custom-select custom-select-lg" style="width: 10%" >
+            <option selected disabled>------</option>
+            <?php 
+                foreach($software as $option) {
+                    echo "<option value='", $option["ComponentTypeID"],"' > ", $option["ComponentTypeName"], " </option>";
                 }        
             ?>
         </select>
@@ -55,21 +122,30 @@
         <?php
 
             if(isset($_POST["search"])) {
-                if(!isset($_POST['selection'])) {                    
-                    return;
-                }
-            $result = getComponentsForRoom($_POST["selection"]);
-            $tableConfig = array(
-                'columns' => array(
-                    'Raum' => 'Raum',
-                    'ComponentName' => 'Name',
-                    'Kategorie' => 'Kategorie',
-                ),
-                'pageName' => 'reporting',
-                'idColumn' => 'ComponentId',
-                'result' => $result,
-            );     
-        }        
+            if(isset($_POST["room"])) {
+                $result = getComponentsForRoom($_POST["room"]);
+            }
+            if(isset($_POST["hardware"])) {
+                $result = getComponentByTypeId($_POST["hardware"]);
+            } 
+            if(isset($_POST["software"])) {
+                $result = getComponentByTypeId($_POST["software"]);
+            }
+            if(isset($_POST["room"]) || isset($_POST["hardware"]) || isset($_POST["software"])) {
+
+                
+                $tableConfig = array(
+                    'columns' => array(
+                        'Raum' => 'Raum',
+                        'ComponentName' => 'Name',
+                        'Kategorie' => 'Kategorie',
+                    ),
+                    'pageName' => 'reporting',
+                    'idColumn' => 'ComponentId',
+                    'result' => $result,
+                );     
+            }        
+        }    
     ?>
     <?php if(isset($tableConfig) && mysqli_num_rows($result) > 0): ?>
     <div class="clearfix">
