3aa4fe7882a3385c0c9ed80c6ac35f91a4850473
Rall3n <nb.rallen@gmail.com>
2019-07-03 09:55:49 +0200

    Einheitliche Dokumentation und Suche für Komponenten

--- src/pages/component.php
+++ src/pages/component.php
@@ -1,36 +1,39 @@
 <?php
-#Hole alle Einträge aus der Tabelle Components
+    /**
+     * Liste aller Kompontenen
+     * 
+     * @author Maximilan Bachhuber, Jonas Becker
+     */
 
-$query = "SELECT * FROM components";
+    $query = "SELECT * FROM components";
 
-$result = mysqli_query($dbLink,$query);
+    if (isset($_GET['search']) and !empty($_GET['search'])) {
+        $query = " WHERE ComponentName LIKE '%".$_GET['search']."%'";
+    }
 
-# Zähle alle Einträge
+    $result = mysqli_query($dbLink,$query);
 
+    $tableConfig = array(
+        'columns' => array(
+            'ComponentID' => '#',
+            'ComponentName' => 'Komponentenname',
+            'SupplierID' => 'LieferantenID',
+            'ComponentPurchaseDate' => 'Kaufdatum',
+            'ComponentWarranty' => 'Garantie',
+            'ComponentNotes' => 'Bemerkung',
+            'ComponentVendorID' => 'VerkäuferID',
+            'ComponentTypeID' => 'KomponentenartID'
+        ),
+        'singularName' => 'Komponenten',
+        'IDColumn' => 'ComonentID',
+        'pageName' => 'component',
+        'result' => $result
+    );
 ?>
 
 <h1>Stammdaten - Komponenten</h1>
 <div class="card">
     <div class="card-body">
-        <?php
-            $tableConfig = array(
-                'columns' => array(
-                    'ComponentID' => '#',
-                    'ComponentName' => 'Komponentenname',
-                    'SupplierID' => 'LieferantenID',
-					'ComponentPurchaseDate' => 'Kaufdatum',
-					'ComponentWarranty' => 'Garantie',
-					'ComponentNotes' => 'Bemerkung',
-					'ComponentVendorID' => 'VerkäuferID',
-					'ComponentTypeID' => 'KomponentenartID'
-                ),
-                'singularName' => 'Komponenten',
-                'IDColumn' => 'ComonentID',
-                'pageName' => 'component',
-                'result' => $result
-            );
-
-            include_once('./templates/table.php');
-        ?>
+        <?php include_once('./templates/table.php'); ?>
     </div>
 </div>
