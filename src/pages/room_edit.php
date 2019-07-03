bcdefbf3b343c1f0aee49f791ae2af90daef0ef5
PizzaService <benni.owa@web.de>
2019-07-03 12:10:37 +0200

    make rooms more readable

--- src/pages/room_edit.php
+++ src/pages/room_edit.php
@@ -12,12 +12,12 @@ if (!isset($_GET["id"])) {
     exit;
 }
 
-$id = $_GET["id"];
+$id = (isset($_GET['id']) and !empty($_GET['id'])) ? intval($_GET['id']) : -1;
 $query = "SELECT * FROM rooms WHERE RoomID = $id;";
 $result = mysqli_query($dbLink, $query);
+$room = mysqli_fetch_assoc($result);
 
-if (mysqli_num_rows($result) !== 0) {
-    $room = mysqli_fetch_assoc($result);
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     if (isset($_POST["submit-edit-room"])) {
 
         $roomName = $_POST["input-room-name"];
@@ -25,37 +25,29 @@ if (mysqli_num_rows($result) !== 0) {
         $roomNodes = $_POST["input-room-description"];
         $valid = true;
 
-        if (ctype_space($roomNumber)) {
-            echo "<div class='alert alert-danger' role='alert'> " .
-            "Bitte geben Sie eine Raumnummer ein! " . 
-          "</div>";
-            $valid = false;
-        }
-        if (ctype_space($roomName)) {
-            echo "<div class='alert alert-danger' role='alert'> " .
-            "Bitte geben Sie einen Raumnamen ein! " . 
-            "</div>";
-            $valid = false;
-        }
         if (strlen($roomNumber) !== 4) {
             echo "<div class='alert alert-danger' role='alert'> " .
-            "Bitte geben Sie beim der Raumnummer genau 4 Zeichen an. " . 
-            "</div>";
+                "Bitte geben Sie beim der Raumnummer genau 4 Zeichen an. " .
+                "</div>";
             $valid = false;
         }
 
         if ($valid) {
             $roomId = $room['RoomID'];
-            $query = "UPDATE rooms SET RoomNo = '$roomNumber', RoomName = '$roomName', RoomNodes='$roomNodes' WHERE RoomID = $roomId";
-            $resultUpdate = mysqli_query($dbLink, $query);
+            $queryUpdate = "UPDATE rooms SET RoomNo = '$roomNumber', RoomName = '$roomName', RoomNodes='$roomNodes' WHERE RoomID = $roomId";
+            $resultUpdate = mysqli_query($dbLink, $queryUpdate);
 
-            if ($resultUpdate === false) {
-                echo "<p>Upps da tratt ein Fehler auf. Versuchen Sie es bitte erneut.</p>";
+            if ($resultUpdate) {
+                echo "<div class='alert alert-success'>Änderungen erfolgreich gespeichert.</div>";
+                $result = mysqli_query($dbLink, $query);
+                $room = mysqli_fetch_assoc($result);
             } else {
-                header("Location: index.php?page=$currentPage");
-                exit;
+                echo '<div class="alert alert-danger">Leider tratt bei der Verarbeitung ein Fehler auf, bitte versuchen Sie es später erneut.</div>';
             }
         }
+    }
+    else if(isset($_POST["submit-delete-room"])) {
+
     }
 }
 
@@ -70,42 +62,38 @@ endif;
 if (mysqli_num_rows($result) !== 0) :
     ?>
 
-    <h1>Stammdaten - Räume - Raum bearbeiten</h1>
+    <h1>Stammdaten - Räume - <?php echo $room['RoomName']; ?></h1>
 
-    <div class="container" style="margin-top:30px;">
-        <form method="POST">
-            <div class="form-group row">
-                <label for="input-room-id" class="col-sm-2 col-form-label">Raum-Id</label>
-                <div class="col-sm-7">
-                    <input type="text" class="form-control" id="input-room-id" name="input-room-id" disabled value="<?php echo $room['RoomID']; ?>" />
+    <div class="card">
+        <div class="card-body" style="background-color:#f8f9fa;">
+            <form method="POST">
+                <div class="form-group row">
+                    <label for="input-room-id" class="col-sm-2 col-form-label">Raum-Id</label>
+                    <div class="col-sm-7">
+                        <input type="text" class="form-control" id="input-room-id" name="input-room-id" disabled value="<?php echo $room['RoomID']; ?>" />
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <label for="input-room-number" class="col-sm-2 col-form-label">Raumnummer</label>
-                <div class="col-sm-7">
-                    <input type="text" class="form-control" id="input-room-number" value="<?php echo $room['RoomNo']; ?>" name="input-room-number" maxlength="4" required>
+                <div class="form-group row">
+                    <label for="input-room-number" class="col-sm-2 col-form-label">Raumnummer</label>
+                    <div class="col-sm-7">
+                        <input type="text" class="form-control" id="input-room-number" value="<?php echo $room['RoomNo']; ?>" name="input-room-number" maxlength="4" required>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <label for="input-room-name" class="col-sm-2 col-form-label">Raumname</label>
-                <div class="col-sm-7">
-                    <input type="text" class="form-control" id="input-room-name" value="<?php echo $room['RoomName']; ?>" name="input-room-name" maxlength="45" required>
+                <div class="form-group row">
+                    <label for="input-room-name" class="col-sm-2 col-form-label">Raumname</label>
+                    <div class="col-sm-7">
+                        <input type="text" class="form-control" id="input-room-name" value="<?php echo $room['RoomName']; ?>" name="input-room-name" maxlength="45" required>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <label for="input-room-description" class="col-sm-2 col-form-label">Raumbeschreibung</label>
-                <div class="col-sm-7">
-                    <textarea class="form-control" id="input-room-description" rows="3" name="input-room-description" maxlength="500"><?php echo $room['RoomNodes']; ?></textarea>
+                <div class="form-group row">
+                    <label for="input-room-description" class="col-sm-2 col-form-label">Raumbeschreibung</label>
+                    <div class="col-sm-7">
+                        <textarea class="form-control" id="input-room-description" rows="3" name="input-room-description" maxlength="500"><?php echo $room['RoomNodes']; ?></textarea>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-sm-2">
-                    <button type="submit" class="btn btn-primary" name="submit-edit-room">Speichern</button>
-                </div>
-                <div class="col-sm-3">
-                    <a class="btn btn-secondary" href="index.php?page=room">Abbrechen</a>
-                </div>
-            </div>
-        </form>
+                <button type="submit" class="btn btn-success" name="submit-edit-room">Speichern</button>
+                <button type="submit" class="btn btn-danger" name="submit-delete-room">Löschen</button>
+            </form>
+        </div>
     </div>
 <?php endif; ?>
--- src/pages/room_new.php
+++ src/pages/room_new.php
@@ -1,39 +1,36 @@
 <?php
+
 /**
  * Auf dieser Seite hat der Nutzer die Möglichkeit, einen neuen Raum anzulegen. Es wird geprüft ob alle 
  * Pflichtfelder gesetzt sind oder nicht. 
  */
-if (isset($_POST["submit-new-room"])) {
+
+$roomnumber = "";
+$roomname = "";
+$roomdescription = "";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit-new-room'])) {
     $roomnumber = $_POST["input-room-number"];
     $roomname = $_POST["input-room-name"];
     $roomdescription = $_POST["input-room-description"];
     $valid = true;
 
-    if (ctype_space($roomnumber)) {
-        echo "<div class='alert alert-danger' role='alert'> " .
-                "Bitte geben Sie eine Raumnummer ein! " . 
-              "</div>";
-        $valid = false;
-    }
-    if (ctype_space($roomname)) {
-        echo "<div class='alert alert-danger' role='alert'> " .
-                "Bitte geben Sie einen Raumnamen ein! " . 
-                "</div>";
-        $valid = false;
-    }
     if (strlen($roomnumber) !== 3) {
         echo "<div class='alert alert-danger' role='alert'> " .
-                "Bitte geben Sie beim der Raumnummer genau 3 Zahlen an. " . 
-                "</div>";
+            "Bitte geben Sie beim der Raumnummer genau 3 Zahlen an. " .
+            "</div>";
         $valid = false;
     }
 
-    if ($valid) { 
+    if ($valid) {
         $query = "INSERT INTO rooms (RoomNo, RoomName, RoomNodes) VALUES ('R$roomnumber', '$roomname', '$roomdescription');";
         $result = mysqli_query($dbLink, $query);
 
-        if(!$result) {
-            echo "<p>Leider tratt bei der Verarbeitung ein Fehler auf, bitte versuchen Sie es später erneut!</p>";
+        if ($result) {
+            $id = mysqli_insert_id($dbLink);
+            echo "<div class='alert alert-success'>Raum erfolgreich angelegt. <a href='index.php?page=room&detail=edit&id=$id'>Zur Detailansicht</a></div>";
+        } else {
+            echo '<div class="alert alert-danger">Leider tratt bei der Verarbeitung ein Fehler auf, bitte versuchen Sie es später erneut.</div>';
         }
     }
 }
@@ -41,33 +38,38 @@ if (isset($_POST["submit-new-room"])) {
 
 <h1>Stammdaten - Räume - Neuen Raum anlegen</h1>
 
-<div class="container" style="margin-top:30px;">
-    <form method="POST">
-        <div class="form-group row">
-            <label for="input-room-number" class="col-sm-2 col-form-label">Raumnummer</label>
-            <div class="col-sm-7">
-                <input type="number" class="form-control" id="input-room-number" placeholder="Raumnummer z.B. 001" name="input-room-number" maxlength="3" required>
+<div class="card">
+    <div class="card-body" style="background-color:#f8f9fa;">
+        <form method="POST">
+            <div class="form-group row">
+                <label for="input-room-number" class="col-sm-2 col-form-label">Raumnummer</label>
+                <div class="col-sm-7">
+                    <input type="number" class="form-control" id="input-room-number" 
+                            placeholder="Raumnummer z.B. 001" name="input-room-number" maxlength="3" 
+                            required value="<?php echo $roomnumber; ?>">
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label for="input-room-name" class="col-sm-2 col-form-label">Raumname</label>
-            <div class="col-sm-7">
-                <input type="text" class="form-control" id="input-room-name" placeholder="Raumname z.B. Computer-Lab" name="input-room-name" maxlength="45" required>
+            <div class="form-group row">
+                <label for="input-room-name" class="col-sm-2 col-form-label">Raumname</label>
+                <div class="col-sm-7">
+                    <input type="text" class="form-control" id="input-room-name" 
+                            placeholder="Raumname z.B. Computer-Lab" name="input-room-name" maxlength="45" 
+                            required value="<?php echo $roomname; ?>">
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label for="input-room-description" class="col-sm-2 col-form-label">Raumbeschreibung</label>
-            <div class="col-sm-7">
-                <textarea class="form-control" id="input-room-description" rows="3" placeholder="Fügen Sie hier die Beschreibung für den Raum ein..." name="input-room-description" maxlength="500"></textarea>
+            <div class="form-group row">
+                <label for="input-room-description" class="col-sm-2 col-form-label">Raumbeschreibung</label>
+                <div class="col-sm-7">
+                    <textarea   class="form-control" id="input-room-description" rows="3" 
+                                placeholder="Fügen Sie hier die Beschreibung für den Raum ein..." 
+                                name="input-room-description" maxlength="500"><?php echo $roomdescription ?></textarea>
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <div class="col-sm-1">
-                <button type="submit" class="btn btn-primary" name="submit-new-room">Anlegen</button>
+            <div class="form-group row">
+                <div class="col-sm-1">
+                    <button type="submit" class="btn btn-success" name="submit-new-room">Speichern</button>
+                </div>
             </div>
-            <div class="col-sm-6">
-                <button type="reset" class="btn btn-secondary">Zurücksetzen</button>
-            </div>
-        </div>
-    </form>
+        </form>
+    </div>
 </div>
