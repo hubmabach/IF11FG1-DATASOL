2d2f510994db7ada23339eacff325336fe645097
Rall3n <nb.rallen@gmail.com>
2019-07-04 11:17:11 +0200

    Kleinere Verbesserungen und Dokumentation

--- src/assets/scripts/attributes.js
+++ src/assets/scripts/attributes.js
@@ -9,6 +9,7 @@
         $(e.target).parents('label').toggleClass('card-selected', e.target.checked);
     });
 
+    // Führe folgende Funktion jedes mal aus, wenn der "Attribute hinzufügen" Button gedrückt wurde.
     $('#add_attribute_btn').on('click', function(e) {
         e.preventDefault();
         e.stopPropagation();
@@ -19,11 +20,15 @@
         $value = inputElem.val();
         errorElem.hide();
 
+        // Sollte eine Text eingegeben worden sein, dann setze die Funktion fort.
         if ($value) {
+            // FormData gleicht dem Objekt welches erstellt wird, wenn ein normales Formular abgesendet wird.
             var fd = new FormData();
 
+            // Füge den eingegebenen Text zur FormData hinzu.
             fd.append('attribute_name', $value);
 
+            // Sende eine HTTP-Anfrage über JavaScript an die angegebene URL mit FormData
             $.ajax({
                 url: "./ajax_component_attributes.php",
                 data: fd,
@@ -31,16 +36,29 @@
                 contentType: false,
                 processData: false,
                 success: function(data) {
-                    var item = $('label.card').last().parent().clone();
-                    item.find('label').removeClass('card-selected');
+                    // Sollte der Server einen Erfolg (HTTP-Status-Code 201) zurückgeben
+
+                    // Erstelle das Auswahlelement und setze alle benötigten Attribute und Werte
+                    var item =$('<div class="col-sm-4">\
+                            <label class="card ">\
+                                <div class="card-body">\
+                                    <input type="checkbox" name="c_attributes[]" id="" value="">\
+                                    <span></span>\
+                                </div>\
+                            </label>\
+                        </div>');
                     item.find('input').val(data.id);
+                    item.find('input').attr('id', 'c_attributes_' + data.id)
                     item.find('span').text(data.name);
 
+                    // Füge das Auswahlelement zum Dokument hinzu.
                     $('label.card').parents('.row').append(item);
-                    item.find('input').prop('checked', false);
+
+                    // Setze das Textfeld zurück
                     inputElem.val("");
                 },
                 error: function(jqXHR) {
+                    // Sollte der Server einen Fehler melden (HTTP-Status-Code 400 oder 500)
                     if (jqXHR.responseText) {
                         var errorText = jQuery.parseJSON(jqXHR.responseText).error;
                         var errorElem = $('#add_attribute_err');
--- src/templates/html_end.php
+++ src/templates/html_end.php
@@ -13,5 +13,6 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script src="./assets/scripts/navbar.js"></script>
         <script src="./assets/scripts/attributes.js"></script>
+        <script src="./assets/scripts/fileupload.js"></script>
     </body>
 </html>
