e565075d6ed876ff6e0fd12850cb3e67e9538b97
Rall3n <nb.rallen@gmail.com>
2019-07-04 10:10:18 +0200

    Component editing, IsInMaintenance in component list

--- src/pages/component_edit.php
+++ src/pages/component_edit.php
@@ -1,12 +1,15 @@
 <?php
   /**
-   * Formular zur Erstellung neuer Komponenten.
+   * Formular zur Bearbeitung bestehender Komponenten.
    *
-   * Erstellt eine dynamische Liste von Eingabefeldern je nachdem, welcher Komponententyp ausgewählt wurde.
+   * Die Eingabefelder für die Attribute werden abhängig von der in der Datenbank gespeicherten Komponentenart generiert und mit bestehenden Werten ausgefüllt.
    *
-   * Lädt eine Datei bei Vorhandensein in den Ordner `uploads` hoch.
+   * Lädt eine Datei bei Vorhandensein in den Ordner `uploads` hoch und ersetzt die in der Datenbank eingetragene Datei.
    *
-   * @author Maximilian Bachhuber, Jonas Becker
+   * Des Weiteren kann hier eine Kompente ausgemustert werden. Sollte sie ausgemustert worden sein, kann sie wieder eingeführt werden.
+   * Die Kompente kann nur gelöscht werden, wenn sie ausgemustert wurde.
+   * 
+   * @author Nikolas Bayerschmidt
    */
 
   if (!isset($_GET['id']) or empty($_GET['id'])) {
@@ -16,54 +19,112 @@
 
   $valid = true;
 
-  // if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['component_save'])) {
-  //   $component_type_id = $_POST['component_type'];
-  //   $component_name = $_POST['component_name'];
-  //   $component_supplier_id = $_POST['component_supplier'];
-  //   $component_purchase = $_POST['component_purchase'];
-  //   $component_warranty = $_POST['component_warranty'];
-  //   $component_vendor_id = $_POST['component_vendor'];
-  //   $component_room_id = $_POST['component_room'];
-  //   $component_receipt = (empty($_FILES) or !isset($_FILES['component_receipt'])) ? "" : $_FILES['component_receipt'];
-  //   $component_notes = $_POST['component_notes'];
-  //   $component_attributes = $_POST['component_attributes'];
+  if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (isset($_POST['component_save'])) {
+      $component_type_id = $_POST['component_type'];
+      $component_name = $_POST['component_name'];
+      $component_supplier_id = $_POST['component_supplier'];
+      $component_purchase = $_POST['component_purchase'];
+      $component_warranty = $_POST['component_warranty'];
+      $component_vendor_id = $_POST['component_vendor'];
+      $component_room_id = $_POST['component_room'];
+      $component_receipt = $_POST['component_receipt_current'];
+      $component_notes = $_POST['component_notes'];
+      $component_attributes = $_POST['component_attributes'];
 
-  //   if ($component_receipt !== "" && file_exists($component_receipt['tmp_name']) && is_uploaded_file($component_receipt['tmp_name'])) {
-  //     $upload_dir = __dir__ . "/../uploads/";
-  //     $filename = basename( substr(hash("md5", time(), FALSE), 0, 5) ."_". $component_receipt['name']);
+      if (!empty($_FILES) && isset($_FILES['component_receipt']) && file_exists($_FILES['component_receipt']['tmp_name']) && is_uploaded_file($_FILES['component_receipt']['tmp_name'])) {
+        $upload_dir = __dir__ . "/../uploads/";
+        $filename = basename( substr(hash("md5", time(), FALSE), 0, 5) ."_". $_FILES['component_receipt']['name']);
 
-  //     if (move_uploaded_file($component_receipt['tmp_name'], $upload_dir . $filename)) {
-  //       $component_receipt = $filename;
-  //     } else {
-  //       // FIXME: Fehler
-  //       $valid = false;
-  //     }
-  //   }
-  //   if ($valid) {
-  //     $query = "INSERT INTO components (ComponentTypeID, ComponentName, SupplierID, ComponentPurchaseDate, ComponentWarranty, ComponentNotes, ComponentVendorID, ComponentReceipt)
-  //     VALUES ($component_type_id, '$component_name', $component_supplier_id, '$component_purchase', '$component_warranty', '$component_notes', $component_vendor_id, '$component_receipt')";
-  //     $result = mysqli_query($dbLink, $query);
+        if (move_uploaded_file($_FILES['component_receipt']['tmp_name'], $upload_dir . $filename)) {
+          if ($component_receipt !== "") unlink($upload_dir . $component_receipt);
+          $component_receipt = $filename;
+        } else {
+          echo '<div class="alert alert-danger">Leider tratt bei der Verarbeitung ein Fehler auf, bitte versuchen Sie es später erneut.</div>';
+          $valid = false;
+        }
+      }
+      if ($valid) {
+        $saveData = array(
+          'ComponentName' => $component_name,
+          'SupplierID' => $component_supplier_id,
+          'ComponentPurchaseDate' => $component_purchase,
+          'ComponentWarranty' => $component_warranty,
+          'ComponentNotes' => $component_notes,
+          'ComponentVendorID' => $component_vendor_id,
+          'ComponentReceipt' => $component_receipt
+        );
 
-  //     if ($result) {
-  //       $component_id = mysqli_insert_id($dbLink);
+        $query = "UPDATE components SET ".sqlUpdateString($saveData)." WHERE ComponentID = ".$_GET['id'];
+        $result = mysqli_query($dbLink, $query);
 
-  //       $room_query = "INSERT INTO componentsinroom (ComponentID, RoomID) VALUES ($component_id, $component_room_id);";
-  //       mysqli_query($dbLink, $room_query);
+        if ($result) {
+          $room_query = "INSERT INTO componentsinroom (ComponentID, RoomID) VALUES (".$_GET['id'].", $component_room_id) ON DUPLICATE KEY UPDATE RoomID = $component_room_id;";
+          mysqli_query($dbLink, $room_query);
 
-  //       foreach($component_attributes as $attribute) {
-  //           if (empty($attribute['value'])) continue;
-  //           $save_query = "INSERT INTO componenthasvalues (ComponentID, AttributeID, AttributeValue) VALUES ($component_id, ".$attribute['id'].", '".$attribute['value']."');";
-  //           mysqli_query($dbLink, $save_query);
-  //       }
+          foreach($component_attributes as $attribute) {
+            if (empty($attribute['value'])) continue;
+            $save_query = "INSERT INTO componenthasvalues (ComponentID, AttributeID, AttributeValue) VALUES (".$_GET['id'].", ".$attribute['id'].", '".$attribute['value']."') ON DUPLICATE KEY UPDATE AttributeValue = '".$attribute['value']."';";
+            mysqli_query($dbLink, $save_query);
+          }
 
-  //       // TODO: Nachricht bei Erfolg mit Link zur Komponente
+          echo "<div class='alert alert-success'>Änderungen erfolgreich gespeichert.</div>";
+        } else {
+          echo '<div class="alert alert-danger">Leider tratt bei der Verarbeitung ein Fehler auf, bitte versuchen Sie es später erneut.</div>';
+        }
+      }
+    } else if (isset($_POST['component_store'])) {
+      // Überprüfe ob die Komponente "ausmusterbar" ist.
+      $check_query = "SELECT IsInMaintenance FROM components WHERE IsInMaintenance = 0 AND ComponentID = ".$_GET['id'];
+      $result = mysqli_query($dbLink, $check_query);
 
-  //       unset($_POST['component_type']);
-  //     } else {
-  //       echo mysqli_error($dbLink);
-  //     }
-  //   }
-  // }
+      if (mysqli_num_rows($result) > 0) {
+        $change_query = "UPDATE components SET IsInMaintenance = 1 WHERE ComponentID = ".$_GET['id'];
+        $result = mysqli_query($dbLink, $change_query);
+
+        if ($result) {
+          echo "<div class='alert alert-success'>Komponente wurde erfolgreich ausgemustert.</div>";
+        } else {
+          echo '<div class="alert alert-danger">Leider tratt bei der Verarbeitung ein Fehler auf, bitte versuchen Sie es später erneut.</div>';
+        }
+      }
+    } else if (isset($_POST['component_delete'])) {
+      $check_query = "SELECT IsInMaintenance FROM components WHERE IsInMaintenance = 1 AND ComponentID = ".$_GET['id'];
+      $result = mysqli_query($dbLink, $check_query);
+
+      if (mysqli_num_rows($result) > 0) {
+        $delete_query = "DELETE FROM components WHERE ComponentID = ".$_GET['id'];
+        $delete_attributes_query = "DELETE FROM componenthasvalues WHERE ComponentID = ".$_GET['id'];
+        $delete_room_query = "DELETE FROM componentsinroom WHERE ComponentID = ".$_GET['id'];
+        mysqli_begin_transaction($dbLink, MYSQLI_TRANS_START_READ_WRITE);
+        mysqli_query($dbLink, $delete_attributes_query);
+        mysqli_query($dbLink, $delete_room_query);
+        mysqli_query($dbLink, $delete_query);
+        $result = mysqli_commit($dbLink);
+
+        if ($result) {
+          header("Location: ./index.php?page=component");
+          die();
+        } else {
+          echo "<div class='alert alert-danger'>Komponente konnte nicht gelöscht werden.</div>";
+        }
+      }
+    } else if (isset($_POST['component_de_store'])) {
+      $check_query = "SELECT IsInMaintenance FROM components WHERE IsInMaintenance = 1 AND ComponentID = ".$_GET['id'];
+      $result = mysqli_query($dbLink, $check_query);
+
+      if (mysqli_num_rows($result) > 0) {
+        $change_query = "UPDATE components SET IsInMaintenance = 0 WHERE ComponentID = ".$_GET['id'];
+        $result = mysqli_query($dbLink, $change_query);
+
+        if ($result) {
+          echo "<div class='alert alert-success'>Komponente wurde erfolgreich wieder eingeführt.</div>";
+        } else {
+          echo '<div class="alert alert-danger">Leider tratt bei der Verarbeitung ein Fehler auf, bitte versuchen Sie es später erneut.</div>';
+        }
+      }
+    }
+  }
 
   $data_query = "SELECT c.*, cir.RoomID FROM components c LEFT JOIN componentsinroom cir ON c.ComponentID = cir.ComponentID WHERE c.ComponentID = ".$_GET['id'];
 
@@ -116,9 +177,9 @@
         </div>
       </div>
       <div class="form-group row">
-        <label class="control-label col-sm-2 text-sm-right">Gewährleistungs<wbr />dauer </label>
+        <label class="control-label col-sm-2 text-sm-right">Gewährleistung bis</label>
         <div class="col-sm-8">
-          <input type="text" id="componet_warranty" name="component_warranty" value="<?php echo $data['ComponentWarranty']; ?>" class="form-control " placeholder="Text input">
+          <input type="date" id="componet_warranty" name="component_warranty" value="<?php echo $data['ComponentWarranty']; ?>" class="form-control">
         </div>
       </div>
       <div class="form-group row">
@@ -136,10 +197,10 @@
         </div>
       </div>
       <div class="form-group row">
-        <label class="control-label col-sm-2 text-sm-right">Raum</label>
+        <label class="control-label col-sm-2 text-sm-right">Raum <?php if ($data['IsInMaintenance']): ?> vor der Ausmusterung<?php endif; ?></label>
         <div class="col-sm-8">
-          <select class="form-control" id="component_room" name="component_room">
-              <option value="" disabled="" selected="">Auswahl</option>
+          <select class="form-control" id="component_room" name="component_room" <?php if ($data['IsInMaintenance']) echo "disabled"; ?>>
+              <option value="" disabled="" selected="">Kein Raum ausgewählt</option>
               <?php
               $query = "SELECT RoomID, RoomName FROM rooms";
               $result = mysqli_query($dbLink,$query);
@@ -153,14 +214,12 @@
       <div class="form-group row">
         <label class="control-label col-sm-2 text-sm-right">Kaufbeleg</label>
         <div class="col-sm-8">
-          <div class="input-group ">
+          <?php if ($data['ComponentReceipt']): ?><a class="btn btn-light mb-2" href="./uploads/<?php echo $data['ComponentReceipt']; ?>" target="_blank" ><?php echo $data['ComponentReceipt']; ?></a><?php endif; ?>
+          <div class="input-group">
               <div class="custom-file">
+                  <input type="hidden" name="component_receipt_current" value="<?php echo $data['ComponentReceipt']; ?>" />
                   <input type="file" class="custom-file-input" id="component_receipt" name="component_receipt" aria-describedby="fileInput">
-                  <label class="custom-file-label" for="fileInput">
-                    <?php if ($data['ComponentReceipt']): ?>
-                    <a href="./uploads/<?php echo $data['ComponentReceipt']; ?>" target="_blank" ><?php echo $data['ComponentReceipt']; ?></a>
-                    <?php else: ?>Datei auswählen...<?php endif; ?>
-                  </label>
+                  <label class="custom-file-label" for="fileInput">Datei auswählen...</label>
               </div>
           </div>
         </div>
@@ -173,7 +232,10 @@
       </div>
       <hr />
       <?php
-        $query = "SELECT DISTINCT a.AttributeID, a.AttributeName, chv.AttributeValue FROM componentattributes a LEFT JOIN componenttypehasattributes cta ON cta.AttributeID = a.AttributeID LEFT JOIN componenthasvalues chv ON chv.AttributeID = a.AttributeID WHERE chv.ComponentID = ".$data['ComponentID'];
+        $query = "SELECT a.AttributeID, a.AttributeName, (SELECT chv.AttributeValue FROM componenthasvalues chv WHERE chv.AttributeID = a.AttributeID AND chv.ComponentID = ".$_GET['id'].") AS AttributeValue
+        FROM componentattributes a
+        LEFT JOIN componenttypehasattributes cta ON cta.AttributeID = a.AttributeID
+        WHERE cta.ComponentTypeID = ".$data['ComponentTypeID'].";";
         $result = mysqli_query($dbLink, $query);
 
         $i = 0;
@@ -182,13 +244,14 @@
         <label class="control-label col-sm-2 text-sm-right"><?php echo $attribute['AttributeName']; ?></label>
         <div class="col-sm-8">
           <input type="hidden" name="component_attributes[<?php echo $i; ?>][id]" value="<?php echo $attribute['AttributeID']; ?>" />
-          <input type="text" name="component_attributes[<?php echo $i++; ?>][value]" placeholder="<?php echo $attribute['AttributeName']; ?>" value="<?php echo $attribute['AttributeValue']; ?>" class="form-control" />
+          <input type="text" name="component_attributes[<?php echo $i++; ?>][value]" value="<?php echo $attribute['AttributeValue']; ?>" class="form-control" />
         </div>
       </div>
       <?php endwhile; ?>
       <input type="submit" class="btn btn-success" name="component_save" value="Speichern" />
-      <input type="submit" class="btn btn-warning" name="component_store" value="Ausmustern" />
-      <input type="submit" class="btn btn-danger" name="component_delete" value="Löschen" />
+      <?php if (!boolval($data['IsInMaintenance'])): ?><input type="submit" class="btn btn-warning" name="component_store" value="Ausmustern" /><?php endif; ?>
+      <?php if (boolval($data['IsInMaintenance'])): ?><input type="submit" class="btn btn-warning" name="component_de_store" value="Wieder Einführen" /><?php endif; ?>
+      <?php if (boolval($data['IsInMaintenance'])): ?><input type="submit" class="btn btn-danger" name="component_delete" value="Löschen" /><?php endif; ?>
     </form>
   </div>
 </div>
