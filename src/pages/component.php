0d1326507250957f416f305dc7fd7ddbd0819b1c
hubmabach <m.a.bachhuber@gmx.de>
2019-07-04 11:34:47 +0200

    Kommentarupdate

--- src/pages/component.php
+++ src/pages/component.php
@@ -1,10 +1,15 @@
 <?php
-    /**
-     * Liste aller Kompontenen
-     * 
-     * @author Maximilan Bachhuber, Jonas Becker
-     */
-
+/**
+ * Anzeige aller Komponenten
+ *
+ * Erstellt eine dynamische Liste von Eingabefeldern je nachdem, welcher Komponententyp ausgewählt wurde.
+ *
+ * Lädt eine Datei bei Vorhandensein in den Ordner `uploads` hoch.
+ *
+ * @author Maximilian Bachhuber, Jonas Becker
+ * Beginn : 03.07.2019 9.00 : 15:00
+ *
+ */
     $query = "SELECT c.ComponentID, c.ComponentName, s.SupplierCompanyName, c.ComponentPurchaseDate, ct.ComponentTypeName, r.RoomName, IF(c.IsInMaintenance, 'Ja', 'Nein') AS IsInMaintenance FROM components c LEFT JOIN supplier s ON c.SupplierID = s.SupplierID LEFT JOIN componenttypes ct ON c.ComponentTypeID = ct.ComponentTypeID LEFT JOIN componentsinroom cir ON c.ComponentID = cir.ComponentID LEFT JOIN rooms r ON cir.RoomID = r.RoomID";
 
     if (isset($_GET['search']) and !empty($_GET['search'])) {
